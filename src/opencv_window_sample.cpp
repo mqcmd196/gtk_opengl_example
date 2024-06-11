d8add0f0a80bb3d37c60d874353fa6b4426700e9
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2024-06-11 18:04:47 +0900

    add using OpenGL in OpenCV sample

--- src/opencv_window_sample.cpp
+++ src/opencv_window_sample.cpp
@@ -1,19 +1,37 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/opengl.hpp>
+#include <opencv2/highgui.hpp>
+#include <GL/gl.h>
+
+void on_opengl(void* param){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glBegin(GL_QUADS);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f( 0.5f, -0.5f);
+    glVertex2f( 0.5f,  0.5f);
+    glVertex2f(-0.5f,  0.5f);
+    glEnd();
+}
+
 
 int main(){
     int width = 640, height = 480;
+    cv::Mat image = cv::Mat(height, width, CV_8UC3, cv::Scalar(255, 0, 0));
 
-    cv::Mat image = cv::Mat::zeros(height, width, CV_8UC3);
+    cv::namedWindow("OpenGL", cv::WINDOW_OPENGL);
+    cv::resizeWindow("OpenGL", width, height);
 
-    cv::Point center(width / 2., height / 2.);
-    int radius = 50;
+    cv::setOpenGlContext("OpenGL");
+    cv::setOpenGlDrawCallback("OpenGL", on_opengl);
 
-    cv::Scalar color(255, 255, 255);
-    int thickness = -1;
-    cv::circle(image, center, radius, color, thickness);
+    while(true){
+        cv::imshow("OpenGL", image);
+        if(cv::waitKey(30) >= 0) break;
+    }
 
-    cv::imshow("Test image", image);
-    cv::waitKey(0);
+    cv::setOpenGlDrawCallback("OpenGL", 0);
+    cv::destroyAllWindows();
 
     return 0;
 }
